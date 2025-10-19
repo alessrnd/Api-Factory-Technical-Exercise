7d4ca611be1084887abd2f081df7aa603f03ba0f
TicTacToe-Master <alessio.tictactoe@outlook.com>
2025-10-19 20:02:26 +0200

    delete findAllByDeletedFalse

--- technicalexercise/src/main/java/com/vaudoiseassurances/technicalexercise/repository/ClientRepository.java
+++ technicalexercise/src/main/java/com/vaudoiseassurances/technicalexercise/repository/ClientRepository.java
@@ -1,13 +1,10 @@
 package com.vaudoiseassurances.technicalexercise.repository;
 
 import com.vaudoiseassurances.technicalexercise.model.Client;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    List<Client> findAllByDeletedFalse();
+    
 }
